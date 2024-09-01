21d4c6f15d4718656eecbd1e18a94e143f03e611
Akshay-Patil94 <patilakshayd15@gmail.com>
2024-09-01 17:28:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function beforesubmit(){
+    //debugger;
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate.value", inputdate.value); // string --> date (en_IN)
+
+    let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formatteddate;
+}
